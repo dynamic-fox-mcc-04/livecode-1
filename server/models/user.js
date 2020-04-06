b176a21230ff512bd47ca3333b0d1d4f2cf3a2cd
AndreRGunawan <andre.graphica@gmail.com>
2020-04-06 09:46:19 +0700

    first commit

---
+++ server/models/user.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  // const User = sequelize.define('User', {
+  //   email: DataTypes.STRING,
+  //   password: DataTypes.STRING
+  // }, {});
+
+  class User extends sequelize.Sequelize.Model {}
+
+  User.init({
+    // Model attributes are defined here
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  
+    // firstName: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false
+    // },
+    // lastName: {
+    //   type: DataTypes.STRING
+    //   // allowNull defaults to true
+    // }
+  }, {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: 'User' // We need to choose the model name
+  });
+
+
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
