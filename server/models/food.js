ede26e1b1e3ff65fbb81610c456ef347b8dad896
bayuaslamaa <bayuaslamaa@gmail.com>
2020-04-06 09:46:38 +0700

    first commit

---
+++ server/models/food.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  // const Food = sequelize.define('Food', {
+  //   title: DataTypes.STRING,
+  //   price: DataTypes.STRING,
+  //   ingredients: DataTypes.STRING,
+  //   tag: DataTypes.STRING,
+  //   UserId: DataTypes.STRING
+  // }, {});
+  class Food extends sequelize.Sequelize.Model { }
+  Food.init({
+    title: DataTypes.STRING,
+    price: DataTypes.STRING,
+    ingredients: DataTypes.STRING,
+    tag: DataTypes.STRING,
+    UserId: {
+      type: DataTypes.STRING,
+      references: sequelize.Sequelize.User
+    }
+  },
+  )
+
+  Food.associate = function (models) {
+    // associations can be defined here
+    Food.belongsTo(models.User)
+  };
+  return Food;
+};
